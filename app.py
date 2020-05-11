d9055f3ea97f971b295b2b833d9943bdf60d0021
Xenia <lommy101@gmail.com>
2020-05-12 07:31:53 +0900

    new commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify, request, redirect, url_for, flash
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'files'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['JWT_SECRET_KEY'] = 'asdf'
+jwt = JWTManager(app)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if username != 'test' or password != 'test':
+        return jsonify({"msg": "Bad username or password"}), 401
+    access_token = create_access_token(identity=username , expires_delta=False)
+    return jsonify(access_token=access_token), 200
+
+@app.route('/api/auth_test', methods=['GET'])
+@jwt_required
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@app.route('/api/upload', methods=['GET', 'POST'])
+@jwt_required
+def upload_file():
+    if request.method == 'POST':
+        file_upload = request.files['file']
+        if file_upload:
+            filename = secure_filename(file_upload.filename)
+            file_upload.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return {'up':'good'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
