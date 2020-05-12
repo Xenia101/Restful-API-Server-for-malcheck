75526e2a6151bcebe1b4742ccf3c3b8bbd9b44e2
Xenia <lommy101@gmail.com>
2020-05-13 00:20:25 +0900

    remove allowed_files

--- app.py
+++ app.py
@@ -5,16 +5,12 @@ import datetime
 import os
 
 UPLOAD_FOLDER = 'files'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['JWT_SECRET_KEY'] = 'asdf'
 jwt = JWTManager(app)
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -49,10 +45,10 @@ def upload_file():
         file_upload = request.files['file']
         if file_upload:
             filename = secure_filename(file_upload.filename)
-            if allowed_file(filename):
-                file_upload.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return {'status':'good'}
-            else: return jsonify({"msg": "not allowed file type"}), 402
-
+            
+            file_upload.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+            return {'status':'good'}
+            
 if __name__ == '__main__':
     app.run(debug=True)
