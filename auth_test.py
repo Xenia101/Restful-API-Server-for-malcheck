f692f741436531a353d9660f0e8a72ce7e5feb4f
XENIA <lommy101@gmail.com>
2020-05-14 11:37:49 +0900

    Update auth_test.py

--- auth_test.py
+++ auth_test.py
@@ -1,6 +1,6 @@
 import requests
 
-API = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODkyOTQ1ODQsIm5iZiI6MTU4OTI5NDU4NCwianRpIjoiOWIzNjA4ZDItMmI5Zi00YmQ0LTk2ZDgtNDY4NDUyZDMwMjFkIiwiZXhwIjoxNjIwODMwNTg0LCJpZGVudGl0eSI6InRlc3QiLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.pqd69R3EkfHM17KDbaoR0K-fREvRJNYImdAAN0_bv1s"
+API = ""
 
 headers = {
     "Authorization" : "Bearer {}".format(API)
