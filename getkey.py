d298a2a250a06fd6e92b36dd978ddb8640557ae6
XENIA <lommy101@gmail.com>
2020-05-17 01:17:10 +0900

    Update getkey.py

--- getkey.py
+++ getkey.py
@@ -2,6 +2,5 @@ import requests
 
 params = {"username":"test", "password":"test"}
 response = requests.post("http://127.0.0.1:5000/login", json=params)
-json_response = response.json()
 
-print(json_response)
+print(response.json())
